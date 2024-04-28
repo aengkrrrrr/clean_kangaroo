e665f5bd52c50c509ac2d7cd71e98ee213b7ee35
sjpark07 <mintgreent@gmail.com>
2024-04-28 21:18:09 +0900

    전 파일에 favicon title 추가 선진

---
+++ admin/php/category/category.php
@@ -0,0 +1,180 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'].'/pinkping/inc/header.php';
+
+
+$sql = "SELECT * FROM category where step = 1";
+$result = $mysqli -> query($sql);
+while($row = $result->fetch_object()){
+  $cate1[]= $row;
+}
+?>
+<div class="container">
+  <form action="">
+     <div class="category row">  
+        <div class="col-md-4">
+            <select class="form-select" aria-label="대분류" id="cate1">
+                <option selected>대분류</option>
+                <?php
+                foreach($cate1 as $c1){
+                 ?>
+                 <option value="<?=$c1->code; ?>"><?=$c1->name; ?></option>
+                <?php
+              }
+               ?>
+            </select>
+        </div>
+        <div class="col-md-4">
+            <select class="form-select" aria-label="중분류" id="cate2">
+            </select>
+        </div>
+        <div class="col-md-4">
+            <select class="form-select" aria-label="소분류" id="cate3">
+            </select>
+        </div>
+            </div>
+      </form>
+      <div class="buttons mt-3">
+<!--대분류 등록 버튼 -->
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#cate1Modal">
+  대분류 등록
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="cate1Modal" tabindex="-1" aria-labelledby="cate1ModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="cate1ModalLabel">대분류 등록</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="modal-body row">
+        <div class="col">
+        <input type="text" class="form-control" id="code1" name="code1" placeholder="코드명 입력">
+        </div>
+        <div class="col">
+        <input type="text" class="form-control" id="name1" name="name1" placeholder="대분류명 입력">
+        </div>
+      </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">닫기</button>
+        <button type="submit" class="btn btn-primary">등록</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!--중분류 등록 버튼 -->
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#cate2Modal">
+  중분류 등록
+</button>
+
+<!--중분류 Modal -->
+<div class="modal fade" id="cate2Modal" tabindex="-1" aria-labelledby="cate2ModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="cate2ModalLabel">중분류 등록</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="modal-body row">
+        <div class="col">
+        <input type="text" class="form-control" id="code2" name="code2" placeholder="코드명 입력">
+        </div>
+        <div class="col">
+        <input type="text" class="form-control" id="name2" name="name2" placeholder="중분류명 입력">
+        </div>
+      </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">닫기</button>
+        <button type="submit" class="btn btn-primary">등록</button>
+      </div>
+    </div>
+  </div>
+</div>
+<!--소분류 등록 버튼 -->
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#cate3Modal">
+  소분류 등록
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="cate3Modal" tabindex="-1" aria-labelledby="cate3ModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="cate3ModalLabel">소분류 등록</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="modal-body row">
+        <div class="col">
+        <input type="text" class="form-control" id="code3" name="code3" placeholder="코드명 입력">
+        </div>
+        <div class="col">
+        <input type="text" class="form-control" id="name3" name="name3" placeholder="소분류명 입력">
+        </div>
+      </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">닫기</button>
+        <button type="submit" class="btn btn-primary">등록</button>
+      </div>
+    </div>
+  </div>
+</div>
+  </div><!--container-->
+<script>
+  $('#cate1').change(function(){
+    makeOption($(this),2,'중분류', $('#cate2'));
+  });
+  $('#cate2').change(function(){
+    makeOption($(this),3,'소분류', $('#cate3'));
+  });
+  $('#cate3').change(function(){
+  });
+
+  function makeOption(e, step, category, target){
+    let cate = e.val();
+   /*비동기 방식으로 printOption 값 3개(cate, step, category) 일시키고, 결과가 나오면 target에 html태그를 생성*/
+   let data = new URLSearchParams({
+    cate:cate,
+    step:step,
+    category:category
+   })
+   console.log(data);
+
+   $.ajax({
+async:false, // success의 결과가 나오면 작업수행
+type:'post',
+data:data,
+url:'printOption.php',
+dataType:'html',
+success:function(result){
+  console.log(result);
+  target.html(result);
+}
+});
+  
+//    $.ajax({
+// 비동기 방식,
+// 타입(get, post...),
+// 넘길 데이터,
+// 대상url,
+// 결과의 형식,
+// 성공하면 할일
+// })
+let categorySubmitBtn =  $(".modal button[type='submit']");
+
+
+
+
+  }
+  </script>
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'].'/pinkping/inc/footer.php';
+?>
