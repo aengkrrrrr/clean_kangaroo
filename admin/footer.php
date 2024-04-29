31cba7a20b2aa8855cdee01e70924ed4be6cb500
songrim <srimm3399@naver.com>
2024-04-29 17:47:24 +0900

    공통_푸터_송림

--- admin/footer.php
+++ admin/footer.php
@@ -5,3 +5,6 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
 <script src="../js/common.js"></script>
+<?= $script1 ?? ''?>
+<!-------------------- 스크립트 -->
+</html>
