b960d50ad2938e91f271159526f91cab408432af
LeeDaYoungss <leedayeonggg@gmail.com>
2024-04-29 17:44:00 +0900

    관리자_이다영_수정

--- admin/coupon/coupon_edit.php
+++ admin/coupon/coupon_edit.php
@@ -1,57 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+  <link href="https://cdn.jsdelivr.net/gh/sun-typeface/SUIT/fonts/static/woff2/SUIT.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  <link rel="icon" href="images/favicon.png" type="image/x-icon">
+  <link rel="stylesheet" href="/clean_kangaroo/css/common.css">
+
+    <!-- datepicker -->
+  <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+  <link rel="stylesheet" href="/resources/demos/style.css">
+
+  <link rel="stylesheet" href="../../css/common.css">
+  <link rel="stylesheet" href="../../css/coupon.css">
+
+  <title>쿠폰관리 | Deep Learning Kangaroo</title>
+</head>
+
 <?php
 session_start();
-include_once $_SERVER['DOCUMENT_ROOT'] . '/clean_kangaroo/php/header.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/clean_kangaroo/admin/header.php';
 ?>
 
-<div class="container grid">
-  <form class="coupon_wrap">
-    <div class="coupon_1 d-flex">
-      <div class="couponimg">
-        <div id="addedimages" class="gap-3 p-3"></div>
-        <input type="file" multiple name="upfile[]" id="upfile" class="d-none">
-        <button type="button" class="btn btn-secondary btn-sm" id="addImage">이미지 첨부하기</button>
-      </div>
-      <div class="coupon_area">
-        <div class="form-floating">
-          <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
-          <label for="floatingTextarea">쿠폰명</label>
+<body>
+  <div class="container grid">
+    <form class="coupon_wrap">
+      <div class="coupon_1 d-flex">
+        <div class="couponimg">
+          <div id="addedimages" class="gap-3 p-3"></div>
+          <input type="file" multiple name="upfile[]" id="upfile" class="d-none">
+          <button type="button" class="btn btn-secondary btn-sm" id="addImage">이미지 첨부하기</button>
         </div>
-        <div class="form-floating">
-          <p>쿠폰 적용 기간</p>
-          <div class="d-flex cDates">
-            <input type="text" id="datepicker1" class="couponC">
-            <input type="text" id="datepicker2" class="couponC">
+        <div class="coupon_area">
+          <div class="form-floating">
+            <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
+            <label for="floatingTextarea">쿠폰명</label>
+          </div>
+          <div class="form-floating">
+            <p>쿠폰 적용 기간</p>
+            <div class="d-flex cDates">
+              <input type="text" id="datepicker1" class="couponC">
+              <input type="text" id="datepicker2" class="couponC">
+            </div>
           </div>
         </div>
       </div>
-    </div>
-    <div class="coupon_2 d-flex">
-      <div class="form-floating">
-        <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
-          <option selected>사용중</option>
-          <option value="1">보류중</option>
-        </select>
-        <label for="floatingSelect">종류</label>
-      </div>
-      <div class="input-group c2form">
+      <div class="coupon_2 d-flex">
         <div class="form-floating">
-          <input type="text" class="form-control" id="floatingInputGroup1" aria-label="coupon service" placeholder="쿠폰 혜택">
-          <label for="floatingInputGroup1">쿠폰 혜택</label>
+          <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
+            <option selected>사용중</option>
+            <option value="1">보류중</option>
+          </select>
+          <label for="floatingSelect">종류</label>
         </div>
-        <span class="input-group-text">%</span>
-      </div>
-      <div class="input-group c2form">
-        <div class="form-floating">
-          <input type="text" class="form-control" aria-label="coupon sale" placeholder="쿠폰 사용조건">
-          <label for="floatingInputGroup1">쿠폰 사용조건</label>
+        <div class="input-group c2form">
+          <div class="form-floating">
+            <input type="text" class="form-control" id="floatingInputGroup1" aria-label="coupon service" placeholder="쿠폰 혜택">
+            <label for="floatingInputGroup1">쿠폰 혜택</label>
+          </div>
+          <span class="input-group-text">%</span>
         </div>
-        <span class="input-group-text">원</span>
+        <div class="input-group c2form">
+          <div class="form-floating">
+            <input type="text" class="form-control" aria-label="coupon sale" placeholder="쿠폰 사용조건">
+            <label for="floatingInputGroup1">쿠폰 사용조건</label>
+          </div>
+          <span class="input-group-text">원</span>
+        </div>
       </div>
-    </div>
-    <button class="primary_btn couponbtn">수정</button>
-    <button class="basic_btn couponbtn">취소</button>
-  </form>          
-</div>
+      <button class="primary_btn couponbtn">수정</button>
+      <button class="basic_btn couponbtn">취소</button>
+    </form>          
+  </div>
+</body>
 
 <script>
   //추가 이미지 등록
@@ -79,5 +102,9 @@ $( function() {
 </script>
 
 <?php
-include_once $_SERVER['DOCUMENT_ROOT'] . '/clean_kangaroo/php/footer.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/clean_kangaroo/admin/footer.php';
 ?>
+<script src="../../js/dashboard.js"></script>
+
+<!-------------------- 스크립트 -->
+</html>
--- admin/coupon/coupon_list.php
+++ admin/coupon/coupon_list.php
@@ -1,81 +1,89 @@
 <?php
 session_start();
-include_once $_SERVER['DOCUMENT_ROOT'] . '/clean_kangaroo/php/header.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/clean_kangaroo/admin/header.php';
+$title = "쿠폰관리";
+$css1 = '<link rel="stylesheet" href="../../css/coupon.css">';
 ?>
 
 
-  <div class="grid">
-    <div class="totalcp">
-      <p>전체 등록 쿠폰리스트  총 n개의 쿠폰이 등록되어 있습니다.</p>
-    </div>
-    <div class="board_container">
-        <div class="board_category df">
-          <div class="select_wrap">
-            <select class="form-select" aria-label="" id="" name="">
-              <option selected>사용중</option>
-              <option>보류중</option>
-            </select>
+  <body>
+    <div class="grid">
+      <div class="totalcp">
+        <p>전체 등록 쿠폰리스트  총 n개의 쿠폰이 등록되어 있습니다.</p>
+      </div>
+      <div class="board_container">
+          <div class="board_category df">
+            <div class="select_wrap">
+              <select class="form-select" aria-label="" id="" name="">
+                <option selected>사용중</option>
+                <option>보류중</option>
+              </select>
+            </div>
+            <div class="search_wrap df">
+              <input class="form-control search" type="text" id="search_keyword" name="keyword">
+              <button class="primary_btn">검색</button>
+            </div>
           </div>
-          <div class="search_wrap df">
-            <input class="form-control search" type="text" id="search_keyword" name="keyword">
-            <button class="primary_btn">검색</button>
-          </div>
-        </div>
-      <hr>  
-      <form action="">
-        <table class="table">
-          <thead>
+        <hr>  
+        <form action="">
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">쿠폰이미지</th>
+                <th scope="col">쿠폰명</th>
+                <th scope="col">행사기간</th>
+                <th scope="col">종류</th>
+                <th scope="col">할인금액(률)</th>
+                <th scope="col">조건</th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
             <tr>
-              <th scope="col">쿠폰이미지</th>
-              <th scope="col">쿠폰명</th>
-              <th scope="col">행사기간</th>
-              <th scope="col">종류</th>
-              <th scope="col">할인금액(률)</th>
-              <th scope="col">조건</th>
-              <th scope="col"></th>
+              <td><img src="/admin/images/test_coupon.png" alt=""></td>
+              <td>회원가입 쿠폰</td>
+              <td>2024.04.22 <br>- 2024.05.22</td>
+              <td>사용중</td>
+              <td>10%</td>
+              <td>10,000원</td>
+              <td class="couponSvg">
+                <a href="coupon_edit.html"><img src="/admin/images/edit.svg" alt=""></a>
+                <a href=""><img src="/admin/images/delete.svg" alt=""></a>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-          <tr>
-            <td><img src="/admin/images/test_coupon.png" alt=""></td>
-            <td>회원가입 쿠폰</td>
-            <td>2024.04.22 <br>- 2024.05.22</td>
-            <td>사용중</td>
-            <td>10%</td>
-            <td>10,000원</td>
-            <td class="couponSvg">
-              <a href="coupon_edit.html"><img src="/admin/images/edit.svg" alt=""></a>
-              <a href=""><img src="/admin/images/delete.svg" alt=""></a>
-            </td>
-          </tr>
-          </tbody>
-        </table>
-      </form>
-      
+            </tbody>
+          </table>
+        </form>
+        
+    </div>
+    </div>
+    <!--공통 pagination-->
+    <div class="nav_wrap df aic">
+      <nav aria-label="">
+        <ul class="pagination">
+          <li class="page-item disabled">
+            <a class="page-link">&laquo;</a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item active" aria-current="page">
+            <a class="page-link" href="#">2</a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item">
+            <a class="page-link" href="#">&raquo;</a>
+          </li>
+        </ul>
+      </nav>
+    <!------------- 공통 pagination-->
+      <a href="coupon_up.php" class="primary_btn">쿠폰 등록</a>
   </div>
-  </div>
-  <!--공통 pagination-->
-  <div class="nav_wrap df aic">
-    <nav aria-label="">
-      <ul class="pagination">
-        <li class="page-item disabled">
-          <a class="page-link">&laquo;</a>
-        </li>
-        <li class="page-item"><a class="page-link" href="#">1</a></li>
-        <li class="page-item active" aria-current="page">
-          <a class="page-link" href="#">2</a>
-        </li>
-        <li class="page-item"><a class="page-link" href="#">3</a></li>
-        <li class="page-item">
-          <a class="page-link" href="#">&raquo;</a>
-        </li>
-      </ul>
-    </nav>
-  <!------------- 공통 pagination-->
-    <a href="coupon_up.html" class="primary_btn">쿠폰 등록</a>
-</div>
+  </body>
 
 
 <?php
-include_once $_SERVER['DOCUMENT_ROOT'] . '/clean_kangaroo/php/footer.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/clean_kangaroo/admin/footer.php';
 ?>
+<script src="../../js/dashboard.js"></script>
+
+<!-------------------- 스크립트 -->
+</html>
