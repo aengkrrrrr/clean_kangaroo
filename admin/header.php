5cecd3c7fc9920df76053624535599ee5eeeba0b
songrim <srimm3399@naver.com>
2024-04-29 17:14:11 +0900

    헤더푸더 변경

--- admin/header.php
+++ admin/header.php
@@ -1,24 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-  <link href="https://cdn.jsdelivr.net/gh/sun-typeface/SUIT/fonts/static/woff2/SUIT.css" rel="stylesheet">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <link rel="icon" href="images/favicon.png" type="image/x-icon">
-  <link rel="stylesheet" href="../../css/common.css">
-
-  <!-- datepicker -->
-  <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
-<link rel="stylesheet" href="/resources/demos/style.css">
-
-  <title><?= $title ?? ''; ?> | Deep Learning Kangaroo</title>
-</head>
 <header id="header">
     <div class="hd_container">
-      <h1 class="logo"><a href="#" class="hidden">메인로고</a></h1>
-      <nav>
+      <h1 class="logo"><img src="/clean_kangaroo/images/admin_header_logo.png" alt=""></h1>
         <ul class="gnb_wrap df">
           <li><a href="" class="body1b">강좌 관리</a></li>
           <li><a href="" class="body1b">게시판 관리</a></li>
@@ -33,10 +15,10 @@
     <div class="common_main_tit">
       <div class="admin_wrap df aic">
         <a href="#" class="bell">
-          <img src="/admin/images/bell_Vector.png" alt="">
+          <img src="/clean_kangaroo/images/bell_Vector.png" alt="">
           <span class="qna_quantity">5</span>
         </a>
-        <span class="kang"><img src="/admin/images/popup_kang_btn.png" alt=""></span>
+        <span class="kang"><img src="/clean_kangaroo/images/popup_kang_btn.png" alt=""></span>
         <span>깨끗한 아기 캥거루</span>
       </div>
       <h4 class="body1b"><?= $title ?? ''; ?></h4>
