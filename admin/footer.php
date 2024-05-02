1815f1d157db44a807013dbdcec1851c91cc9461
songrim <srimm3399@naver.com>
2024-05-02 11:40:35 +0900

    헤더_메뉴_액티브추가/부트스트랩 파일 많이추가

--- admin/footer.php
+++ admin/footer.php
@@ -2,7 +2,8 @@
 <!-- 스크립트 -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="/clean_kangaroo/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> -->
 <script src="/clean_kangaroo/js/common.js"></script>
 <?= $script1 ?? ''?>
 <?= $script2 ?? ''?> 
