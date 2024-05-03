6a8e8aaf55368dd375a10f9136303a46605b9b01
LeeDaYoungss <leedayeonggg@gmail.com>
2024-05-03 18:18:38 +0900

    관리자_이다영_카테고리수정

--- admin/lecture/category.php
+++ admin/lecture/category.php
@@ -5,32 +5,25 @@ $css1 = '<link rel="stylesheet" href="../../css/lecture.css">';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/clean_kangaroo/admin/dbcon.php';
 include_once $_SERVER['DOCUMENT_ROOT'] . '/clean_kangaroo/admin/header.php';
 
-$sql = "SELECT * FROM category where step = 1";
+$sql = "SELECT * FROM product_category where step = 1";
 $result = $mysqli->query($sql);
 while ($row = $result->fetch_object()) {
   $cate1[] = $row;
 }
-
 ?>
 
 <div class="container">
-
   <div class="category row">
-
     <div class="col-md-4">
-
       <select class="form-select" aria-label="대분류" id="cate1">
         <option selected>대분류</option>
         <?php
         foreach ($cate1 as $c1) {
         ?>
-
           <option value="<?= $c1->code; ?>"><?= $c1->name; ?></option>
-
         <?php
         }
         ?>
-
       </select>
     </div>
     <div class="col-md-4">
@@ -39,21 +32,12 @@ while ($row = $result->fetch_object()) {
 
       </select>
     </div>
-    <div class="col-md-4">
-
-      <select class="form-select" aria-label="소분류" id="cate3">
-
-      </select>
-    </div>
-
   </div>
-
   <div class="buttons mt-3">
     <!-- 대분류 등록 버튼 -->
     <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#cate1Modal">
       대분류 등록
     </button>
-
     <!--대분류 등록 Modal -->
     <div class="modal fade" id="cate1Modal" tabindex="-1" aria-labelledby="cate1ModalLabel" aria-hidden="true">
       <div class="modal-dialog">
@@ -71,17 +55,15 @@ while ($row = $result->fetch_object()) {
             </div>
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">닫기</button>
-            <button type="submit" class="btn btn-primary" data-step="1">등록</button>
+            <button type="button" class="btn basic_btn" data-bs-dismiss="modal">닫기</button>
+            <button type="submit" class="btn primary_btn" data-step="1">등록</button>
           </div>
         </div>
       </div>
     </div>
 
     <!-- 중분류 등록 버튼 -->
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#cate2Modal">
-      중분류 등록
-    </button>
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#cate2Modal">중분류 등록</button>
 
     <!-- 중분류 등록 Modal -->
     <div class="modal fade" id="cate2Modal" tabindex="-1" aria-labelledby="cate2ModalLabel" aria-hidden="true">
@@ -114,68 +96,16 @@ while ($row = $result->fetch_object()) {
             </div>
           </div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">닫기</button>
-            <button type="submit" class="btn btn-primary" data-step="2">등록</button>
+            <button type="button" class="btn basic_btn" data-bs-dismiss="modal">닫기</button>
+            <button type="submit" class="btn primary_btn" data-step="2">등록</button>
           </div>
         </div>
       </div>
     </div>
+  </div><!-- //container -->
+</div>
 
-    <!-- 소분류 등록 버튼 -->
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#cate3Modal">
-      소분류 등록
-    </button>
-
-    <!--소분류 등록 Modal -->
-    <div class="modal fade" id="cate3Modal" tabindex="-1" aria-labelledby="cate3ModalLabel" aria-hidden="true">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h1 class="modal-title fs-5" id="cate3ModalLabel">소분류 등록</h1>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-
-            <div class="row">
-              <div class="col">
-                <select class="form-select" aria-label="대분류" id="pcode2_1">
-                  <option selected disabled>대분류를 선택해주세요</option>
-                  <?php
-                  foreach ($cate1 as $c1) {
-                  ?>
-                    <option value="<?= $c1->code; ?>"><?= $c1->name; ?></option>
-                  <?php
-                  }
-                  ?>
-                </select>
-              </div>
-              <div class="col">
-                <select class="form-select" aria-label="중분류" id="pcode3">
-                  <option selected disabled>대분류를 먼저 선택해주세요</option>
-                </select>
-              </div>
-            </div>
-            <div class="row mt-3">
-              <div class="col">
-                <input type="text" class="form-control" id="code3" name="code3" placeholder="코드명 입력">
-              </div>
-              <div class="col">
-                <input type="text" class="form-control" id="name3" name="name3" placeholder="대분류명 입력">
-              </div>
-            </div>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">닫기</button>
-            <button type="submit" class="btn btn-primary" data-step="3">등록</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
-</div><!-- //container -->
-
-<script src="/pinkping/admin/js/makeoption.js"></script>
+<script src="/clean_kangaroo/admin/js/makeoption.js"></script>
 <script>
   let categorySubmitBtn = $(".modal button[type='submit']");
 
