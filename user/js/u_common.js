52f8090e39a9ef7c2b25cee227d594086e7ed18d
songrim <srimm3399@naver.com>
2024-05-08 17:42:59 +0900

    header 검색기능

--- user/js/u_common.js
+++ user/js/u_common.js
@@ -12,12 +12,13 @@ let search = document.querySelector('.search_area');
 searchBtn.addEventListener('click',function(e){
   e.preventDefault();
 	search.classList.toggle('active');
-	
+  
 			if(search.classList.contains('active')) {
 				 search.animate(
 			[
         { height: '0'},
-				{ height: '400px'}
+				{ height: '310px'},
+
 			],
 			{duration:500, fill: "forwards"},
 		);
@@ -25,8 +26,8 @@ searchBtn.addEventListener('click',function(e){
 	} else {
 		 search.animate(
 			[
-        { height: '400px'},
-				{ height: '0px'}
+        { height: '310px'},
+				{ height: '0px'},
 			],
 			{duration:500, fill: "forwards"},
 		);
