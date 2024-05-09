709f162a49966fbbc9b6d5eac93733d4e8817b5c
songrim <srimm3399@naver.com>
2024-05-09 15:17:28 +0900

    공지사항_송림_완성, 전체 셀렉트색상추가

--- user/js/u_common.js
+++ user/js/u_common.js
@@ -1,22 +1,15 @@
-$(function(){
-  $(".user_banner").bxSlider( {
-      mode: 'fade',
-      speed: 400,
-      auto: true,
-      pager: false
-  });
-});
+// 공통 header 검색창
 let searchBtn = document.querySelector('#u_header .u_search');
 let search = document.querySelector('.search_area');
 
 searchBtn.addEventListener('click',function(e){
-  e.preventDefault();
+	e.preventDefault();
 	search.classList.toggle('active');
-  
+	
 			if(search.classList.contains('active')) {
 				 search.animate(
 			[
-        { height: '0'},
+				{ height: '0'},
 				{ height: '310px'},
 
 			],
@@ -26,19 +19,38 @@ searchBtn.addEventListener('click',function(e){
 	} else {
 		 search.animate(
 			[
-        { height: '310px'},
+				{ height: '310px'},
 				{ height: '0px'},
 			],
 			{duration:500, fill: "forwards"},
 		);
 	}
 });
+//////////// 공통 header 검색창
 
-/* 강좌소개--선진 */
+
+  $(".user_banner").bxSlider( {
+      mode: 'fade',
+      speed: 400,
+      auto: true,
+      pager: false
+  });
+
+// 강좌소개 - 선진 
 $('.lec_container').bxSlider({
 	minSlides: 4,
 	maxSlides: 10,
 	slideWidth: 282,
 	slideMargin: 30
 });
-/* ////////////강좌소개--선진 */
+//////////// 강좌소개 - 선진 
+
+
+// 공지사항 - 송림
+$('.notice_wrap').bxSlider({
+	minSlides: 1,
+	pager:false,
+	prevSelector:'.main_notice .controls .prev',
+	nextSelector:'.main_notice .controls .next'
+});
+//////////// 공지사항 - 송림
