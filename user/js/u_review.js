72ddb26fdf176dad3dca93b996c0d59298e59517
LeeDaYoungss <leedayeonggg@gmail.com>
2024-05-13 15:49:17 +0900

    사용자_이다영_리뷰추가

--- user/js/u_review.js
+++ user/js/u_review.js
@@ -1,3 +1,18 @@
+let rating = $('.review .rating');
+let selectForm = $('#makeStar');
+// rating.eq(0).find('i:nth-child(-1+3)').css({color:'#F05522'});
+
+rating.each(function(){
+	let num = $(this).attr('data-rate');
+	$(this).find('i').slice(0,num).css({color:'#F05522'});
+});
+
+selectForm.on('change',function(){
+	let num = $(this).val();
+	$('.make_star .rating').find('svg').css({color:'#ebebeb'});
+	$('.make_star .rating').find('svg').slice(0,num).css({color:'#F05522'});
+});
+
 $(".user_review_list").bxSlider( {
   maxSlides: 4,
   slideWidth: 330,
