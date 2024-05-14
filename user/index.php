6b0f9a8ce30ed94391c1e8e85369408a04e0fddf
songrim <srimm3399@naver.com>
2024-05-14 12:32:35 +0900

    header,footer 수정

--- user/index.php
+++ user/index.php
@@ -346,42 +346,9 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/clean_kangaroo/user/u_header.php';
 
 </main>
 
-<!-- 사용자 푸터 -->
-<footer>
-  <div class="user_countainer">
-    <div class="df u_footer_wrap">
-      <h2><a href="index.php">딥러닝캥거루</a></h2>
-      <ul class="df">
-        <li class="body3">딥러닝캥거루</li>
-        <li class="body3">사업자번호 : 640-81-01354</li>
-        <li class="body3">대표 : 깨끗한 아기 캥거루</li>
-        <li class="body3">개인정보책임자 : 김동주</li>
-        <li class="body3">제휴&마케팅 문의 : dlkang@create.co.kr</li>
-        <li class="body3">Copyright © DEEP LEARNING KANGAROO. All Rights Reserved.</li>
-      </ul>
-      <p class="h4">대표전화<br><strong>1988-8782</strong></p>
-    </div>
-  </div>
-</footer>
-<!--------- 사용자 푸터 -->
-</body>
+<?php
+
+include_once $_SERVER['DOCUMENT_ROOT'] . '/clean_kangaroo/user/footer.php';
 
 
-<!-- 스크립트 -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://unpkg.com/imagesloaded@5/imagesloaded.pkgd.min.js"></script>
-<script src="./js/jquery.bxslider.min.js"></script>
-
-<!-- 카운트다운 -->
-<script src='./js/moment.js'></script>
-<script src='./js/moment-with-locales.js'></script>
-<script src="./js/countdown.js"></script>
-
-<script src="./js/u_main.js"></script>
-<script src="./js/u_common.js"></script>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js" integrity="sha512-ahmSZKApTDNd3gVuqL5TQ3MBTj8tL5p2tYV05Xxzcfu6/ecvt1A0j6tfudSGBVuteSoTRMqMljbfdU0g2eDNUA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-</html>
+?>
