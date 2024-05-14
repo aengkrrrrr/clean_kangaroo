3843d908d23aa8d7aea1f26ebf2d694ae37592df
LeeDaYoungss <leedayeonggg@gmail.com>
2024-05-14 12:42:16 +0900

    사용자_이다영_이벤트리스트php

--- user/event_list.php
+++ user/event_list.php
@@ -1,45 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-  <link href="https://cdn.jsdelivr.net/gh/sun-typeface/SUIT/fonts/static/woff2/SUIT.css" rel="stylesheet">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  <link rel="icon" href="../images/favicon.png" type="image/x-icon">
-  
-  <title>Deep Learning kangaroo | EVENT</title>
+<?php
+$title='홈';
 
-  <link rel="stylesheet" href="../css/common.css">
-  <link rel="stylesheet" href="./css/u_common.css">
-  <link rel="stylesheet" href="./css/u_event.css">
-</head>
-<body class="u_body">
-  <header id="u_header">
-    <div class="header_container df aic">
-        <h1 class="u_logo">
-          <a href="">
-            <img src="/images/admin_header_logo.png" alt="">
-          </a>
-        </h1>
-        <nav class="u_gnb_wrap">
-          <ul class="u_gnb df">
-            <li><a href="" class="body3b">웹디자인/편집</a></li>
-            <li><a href="" class="body3b">웹개발</a></li>
-            <li><a href="" class="body3b">CG/모션그래픽</a></li>
-            <li><a href="" class="body3b">게임/웹툰</a></li>
-            <li><a href="" class="body3b">수강평</a></li>
-            <li><a href="" class="body3b">Q&A</a></li>
-            <li><a href="" class="body3b">공지사항</a></li>
-          </ul>
-        </nav>
-        <div class="util_wrap df aic">
-          <a href=""><span class="material-symbols-outlined"> search</span></a>
-          <a href=""><span class="material-symbols-outlined">shopping_cart</span></a>
-          <a href="">로그인</a>
-        </div>
-    </div>
-  </header>
+include_once $_SERVER['DOCUMENT_ROOT'] . '/clean_kangaroo/admin/dbcon.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/clean_kangaroo/user/u_header.php';
+
+
+
+?>
   <main class="usergrid">
     <div class="user_subevent_title">
       <h2 class="h2">event</h2>
@@ -82,29 +49,9 @@
       </ul>
     </nav>
   </main>
-  <footer>
-    <div class="user_countainer">
-      <div class="df u_footer_wrap">
-        <h2><a href="index.php">딥러닝캥거루</a></h2>
-        <ul class="df">
-          <li class="body3">딥러닝캥거루</li>
-          <li class="body3">사업자번호 : 640-81-01354</li>
-          <li class="body3">대표 : 깨끗한 아기 캥거루</li>
-          <li class="body3">개인정보책임자 : 김동주</li>
-          <li class="body3">제휴&마케팅 문의 : dlkang@create.co.kr</li>
-          <li class="body3">Copyright © DEEP LEARNING KANGAROO. All Rights Reserved.</li>
-        </ul>
-        <p class="h4">대표전화<br><strong>1988-8782</strong></p>
-      </div>
-    </div>
-  </footer>
-</body>
-<!-- 스크립트 -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="./js/jquery.bxslider.min.js"></script>
-<script src="./js/u_main.js"></script>
-<script src="./js/u_common.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-<script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
-</html>
+<?php
+
+include_once $_SERVER['DOCUMENT_ROOT'] . '/clean_kangaroo/user/footer.php';
+
+
+?>
