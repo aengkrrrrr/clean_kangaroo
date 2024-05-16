05b4a98ac1d0a38d8f74a52a576da996460a0667
sjpark07 <mintgreenT@gmail.com>
2024-05-16 17:27:54 +0900

    0516 1725 선진

--- user/u_qna_ok.php
+++ user/u_qna_ok.php
@@ -1,21 +1,19 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . '/clean_kangaroo/admin/dbcon.php';
 
-
-$idx = $_POST['idx'];
-$name = $_POST['name'];
+$name = $_POST['userid'];
 $title = $_POST['title'];
 $content = $_POST['content'];
 $date = date("Y-m-d");
 
-$sql = "INSERT INTO qna_board (idx, name, title, content, date) VALUES ('{$idx}','{$name}','{$title}','{$content}',now())";
+$sql = "INSERT INTO qna_board (userid, title, content, date) VALUES ('{$name}','{$title}','{$content}',now())";
 
-if($mysqli->query($sql) === true){
+if ($mysqli->query($sql) === true) {
   echo "<script>
       alert('Q&A 작성 완료');
       location.href='u_qna_list.php';
       </script>";
-} else{
-  echo "Error:".$sql."<br>".$mysqli->error;
+} else {
+  echo "Error:" . $sql . "<br>" . $mysqli->error;
 }
 $mysqli->close();
