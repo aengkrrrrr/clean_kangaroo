faab29022f327976f02e34e6bb3b5b6873bafdec
songrim <srimm3399@naver.com>
2024-05-17 13:19:05 +0900

    카트 구매하기 진행중_송림

--- user/cart_del.php
+++ user/cart_del.php
@@ -2,13 +2,17 @@
  include_once $_SERVER['DOCUMENT_ROOT'] . '/clean_kangaroo/admin/dbcon.php';
 
  $cartid = $_POST['cartid'];
- $sql = "DELETE FROM cart WHERE cartid = {$cartid}";
+
+ $cartids_str = implode(',', $cartid);
+
+ // SQL 쿼리를 작성합니다.
+ $sql = "DELETE FROM cart WHERE cartid IN ({$cartids_str})";
  $result = $mysqli -> query($sql);
  
  if($result){
-     $data = array('result'=>'ok');
+    $data = array('result'=>'ok');
  }else{
-     $data = array('result'=>'no');
+    $data = array('result'=>'no');
  }
  echo json_encode($data);
 ?>
